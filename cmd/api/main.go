cbc9a8c02ea4f8c642c58f3949c12549f2fa87f8
Justice Nyaga <justiceevansmwema@gmail.com>
2024-04-18 22:12:17 +0300

    Initial Commit

---
+++ cmd/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	// set application config
+	var app application
+
+	app.Domain = "example.com"
+
+	log.Println("Starting application on port", port)
+
+	// read from command line
+
+	// connect to the database
+
+	// start a web server
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module vidly
+
+go 1.21.7
