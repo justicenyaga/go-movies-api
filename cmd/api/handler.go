9e726fbd406cd9c2dfedf21bff53742963b38eff
Justice Nyaga <justiceevansmwema@gmail.com>
2024-04-25 19:51:52 +0300

    Create a route to get all the movies

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+	"vidly/internal/models"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +20,47 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "1986-03-07")
+
+	highlander := models.Movie{
+		ID:          1,
+		Title:       "Highlander",
+		ReleaseDate: rd,
+		RunTime:     116,
+		MPAARating:  "R",
+		Description: "A very nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	rd, _ = time.Parse("2006-01-02", "1981-06-12")
+
+	rotla := models.Movie{
+		ID:          1,
+		Title:       "Raiders of the Lost Ark",
+		ReleaseDate: rd,
+		RunTime:     115,
+		MPAARating:  "PG-13",
+		Description: "Another very nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	movies = append(movies, highlander, rotla)
+
+	out, err := json.Marshal(movies)
 	if err != nil {
 		fmt.Println(err)
 	}
