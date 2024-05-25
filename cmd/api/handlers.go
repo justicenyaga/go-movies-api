eaf527de452afd39dc2ba5fedc58bf201e58dabc
Justice Nyaga <justiceevansmwema@gmail.com>
2024-05-25 15:47:49 +0300

    Create a route to get a movie

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/golang-jwt/jwt/v4"
 )
 
@@ -139,4 +140,22 @@ func (app *application) ManageCatalogue(w http.ResponseWriter, r *http.Request) 
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, movies)
+}
+
+func (app *application) GetMovie(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	movieID, err := strconv.Atoi(id)
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+
+	movie, err := app.DB.OneMovie(movieID)
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, movie)
 }
